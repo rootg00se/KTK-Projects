289c5b702163959610fece6ce66cf87a50435bfa
rootg00se <gorc141408@gmail.com>
2026-04-05 13:10:07 +0300

    frontend setup

---
+++ ktk-client/src/app/App.tsx
@@ -0,0 +1,12 @@
+import type React from "react";
+import { Button } from "@/shared/components/ui/button";
+
+const App: React.FC = () => {
+    return (
+        <div className="wrap">
+            <Button>Test text</Button>
+        </div>
+    );
+};
+
+export default App;
---
+++ ktk-client/src/app/main.tsx
@@ -0,0 +1,19 @@
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { BrowserRouter } from "react-router-dom";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./providers/query-client.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+        <QueryClientProvider client={queryClient}>
+            <BrowserRouter>
+                <App />
+                <ReactQueryDevtools />
+            </BrowserRouter>
+        </QueryClientProvider>
+    </StrictMode>,
+);
