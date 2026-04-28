10ac600a4ced845442b21db795a4c904f018e1f8
rootg00se <gorc141408@gmail.com>
2026-04-28 23:50:00 +0300

    project likes fixed

--- ktk-client/src/entities/project/model/useToggleLike.ts
+++ ktk-client/src/entities/project/model/useToggleLike.ts
@@ -1,44 +1,18 @@
-import type { InfiniteData } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { queryClient } from "@/app/providers/query-client";
-import type { AxiosResponse } from "axios";
 import { toast } from "react-toastify";
 import type { IErrorResponse } from "@/shared/types/error-response.type";
 import { projectsApi } from "../api/project.api";
-import type { IPaginationProjectResponse } from "./types";
-import { useOptimisticMutation } from "@/shared/hooks/useOptimisticMutation";
 
 export const useToggleLike = (like: boolean) => {
-    const toggleLikeMutation = useOptimisticMutation({
+    const toggleLikeMutation = useMutation({
         mutationKey: [projectsApi.baseKey, "toggleLike"],
         mutationFn: like ? projectsApi.likeProject : projectsApi.dislikeProject,
-        cancelQueryKeys: [[projectsApi.baseKey]],
-        optimisticTargets: [
-            {
-                queryFilters: { queryKey: [projectsApi.baseKey, "list"] },
-                updater: (data: unknown, params) => {
-                    const listData = data as InfiniteData<AxiosResponse<IPaginationProjectResponse>> | undefined;
-                    if (!listData) return data;
-
-                    return {
-                        ...listData,
-                        pages: listData.pages.map((page) => ({
-                            ...page,
-                            data: {
-                                ...page.data,
-                                items: page.data.data.map((post) =>
-                                    post.project_id === params.projectId ? { ...post, is_liked: like } : post,
-                                ),
-                            },
-                        })),
-                    };
-                },
-            },
-        ],
         onError: (error: IErrorResponse) => {
             toast.error(error.response.data.message[0] ?? error.response.data.message);
         },
         onSettled: () => {
-            queryClient.invalidateQueries({ queryKey: [projectsApi.baseKey] });
+            queryClient.invalidateQueries({ queryKey: ["projects"] });
         },
     });
 
